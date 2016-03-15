b7ea0a76cc4cc457aaf40b40b0e629bcd6c7fdca
taryo <r.takahashi@rt-net.jp>
2016-03-15 16:42:57 +0900

    no message

--- LPCXpresso Sample Program/workspace/RT-USB-9AXIS-sample_ver2.0_Nikkei_Linux/inc/MODE/mode_calibOmega.h
+++
@@ -1,6 +0,0 @@
-#ifndef MDOE_CALIBOMEGA_H
-#define MDOE_CALIBOMEGA_H
-
-void mode_calibOmega(void);
-
-#endif
--- LPCXpresso Sample Program/workspace/RT-USB-9AXIS-sample_ver2.0_Nikkei_Linux/inc/MODE/mode_clearOffset.h
+++
@@ -1,6 +0,0 @@
-#ifndef MODE_CLEAROFFSET_H
-#define MODE_CLEAROFFSET_H
-
-void mode_clearOffset(void);
-
-#endif
