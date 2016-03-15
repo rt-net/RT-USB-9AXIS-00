1e4e94266df8b11e13fa0702ed832cb4ee37131e
taryo <r.takahashi@rt-net.jp>
2016-03-15 16:29:18 +0900

    no message

--- LPCXpresso Sample Program/workspace/RT-BT-9AXIS-sample/Debug/src/MAIN/tinyMathFunctions.d
+++
@@ -1,8 +0,0 @@
-src/MAIN/tinyMathFunctions.o src/MAIN/tinyMathFunctions.d: \
- ../src/MAIN/tinyMathFunctions.c \
- C:\Users\ryota\OneDrive\RT\ working\github\RT-USB-9AXIS-00\LPCXpresso\ Sample\ Program\workspace\RT-BT-9AXIS-sample\inc\OTHER/type.h \
- C:\Users\ryota\OneDrive\RT\ working\github\RT-USB-9AXIS-00\LPCXpresso\ Sample\ Program\workspace\RT-BT-9AXIS-sample\inc\MAIN/tinyMathFunction.h
-
-C:\Users\ryota\OneDrive\RT\ working\github\RT-USB-9AXIS-00\LPCXpresso\ Sample\ Program\workspace\RT-BT-9AXIS-sample\inc\OTHER/type.h:
-
-C:\Users\ryota\OneDrive\RT\ working\github\RT-USB-9AXIS-00\LPCXpresso\ Sample\ Program\workspace\RT-BT-9AXIS-sample\inc\MAIN/tinyMathFunction.h:
--- LPCXpresso Sample Program/workspace/RT-USB-9AXIS-sample-COMPATIBLE-VER_1_0/inc/MODE/mode_viewAllData.h
+++
@@ -1,6 +0,0 @@
-#ifndef MDOE_VIEWALLDATA_H
-#define MDOE_VIEWALLDATA_H
-
-void mode_viewAllData(void);
-
-#endif
